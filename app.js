0b95ab27093179df5e764487b52a285ca87d4dc1
Lindsay Williams <williamsl25@yahoo.com>
2015-11-03 16:37:59 -0500

    added sendChatToServer function and createNewChat function

--- app.js
+++ app.js
@@ -14,5 +14,31 @@ var chatPage = {
   initEvents: function(){
 
   },
-  
+  createNewChat: function(user, img, msg){
+    event.preventDefault();
+    var newChat = {
+      userName: user,
+      img: img,
+      msg: msg
+    };
+    chatPage.sendChatToServer(newChat);
+
+  },
+  sendChatToServer: function(msg){
+    console.log("Transmitting", msg);
+    $.ajax({
+      url: chatPage.url,
+      method: 'POST',
+      data: msg,
+      success: function(response){
+        console.log("success", response);
+      },
+      failure: function(response){
+        console.log("failure", response);
+      },
+    });
+
+  },
+  url: "https://tiny-tiny.herokuapp.com/collections/perlman/",
+
 };
